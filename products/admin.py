8b60d9d347fbd4ebf279feda58d83fbd4c73de89
Darren Longhurst <dmlonghurst@icloud.com>
2026-01-19 15:49:24 +0000

    customise admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 from .models import Product, Category, ProductVariant
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(ProductVariant)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'is_special_offer')
+
+    ordering = ('sku', 'name')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        )
+
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'variant_type',
+        'variant_value',
+        'price_modifier',
+        'stock_count',
+    )
+    ordering = ('product', 'variant_type', 'variant_value')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductVariant, ProductVariantAdmin)
