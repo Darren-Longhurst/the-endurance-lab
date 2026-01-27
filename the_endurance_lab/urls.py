524a90ba1e36c76b2292f66a1ddb23f419f34a98
Darren Longhurst <dmlonghurst@icloud.com>
2026-01-27 01:37:05 +0000

    Remove references to media_url for cloudinary serving files

--- the_endurance_lab/urls.py
+++ the_endurance_lab/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
