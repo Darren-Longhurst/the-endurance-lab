d85a19221cfc431d84fbc7aa8142fd788ef9b678
Darren Longhurst <dmlonghurst@icloud.com>
2026-01-28 00:10:07 +0000

    css updates

--- the_endurance_lab/settings.py
+++ the_endurance_lab/settings.py
@@ -232,7 +232,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-    DEFAULT_FROM_EMAIL = 'theendurancelab@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
